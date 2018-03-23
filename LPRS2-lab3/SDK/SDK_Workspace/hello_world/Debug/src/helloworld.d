891dc4cb853f193ba76221df790d8a47ccc18f48
student <student@domain.local>
2018-03-23 10:10:32 +0100

    lab03 - the first part

---
+++ LPRS2-lab3/SDK/SDK_Workspace/hello_world/Debug/src/helloworld.d
@@ -0,0 +1,15 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../hello_world_bsp/microblaze_0/include/xio.h \
+ ../../hello_world_bsp/microblaze_0/include/xbasic_types.h \
+ ../../hello_world_bsp/microblaze_0/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../hello_world_bsp/microblaze_0/include/xio.h:
+
+../../hello_world_bsp/microblaze_0/include/xbasic_types.h:
+
+../../hello_world_bsp/microblaze_0/include/xparameters.h:
