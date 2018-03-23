ea8e7f4b9aa01a1c232ba91aa8e4a7810e7c4e82
student <student@domain.local>
2018-03-23 10:29:01 +0100

    may the source be with us...vhdl added.

---
+++ LPRS2-lab3/drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
@@ -0,0 +1,11 @@
+##############################################################################
+## Filename:          D:\ra41-2015\lprs\lab03\lab03_new_hope\LPRS2-lab3/drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
+## Description:       Microprocess Driver Command (tcl)
+## Date:              Fri Mar 23 10:23:01 2018 (by Create and Import Peripheral Wizard)
+##############################################################################
+
+#uses "xillib.tcl"
+
+proc generate {drv_handle} {
+  xdefine_include_file $drv_handle "xparameters.h" "my_peripheral" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" 
+}
---
+++ LPRS2-lab3/drivers/my_peripheral_v1_00_a/src/my_peripheral.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          D:\ra41-2015\lprs\lab03\lab03_new_hope\LPRS2-lab3/drivers/my_peripheral_v1_00_a/src/my_peripheral.c
+* Version:           1.00.a
+* Description:       my_peripheral Driver Source File
+* Date:              Fri Mar 23 10:23:01 2018 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "my_peripheral.h"
+
+/************************** Function Definitions ***************************/
+
